a0fa491bb052d0ad49d9da9b1a81706b78c7c36c
Sheikh Mahmudul Hasan Shium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2021-07-05 10:50:09 +0600

    finalterm added

---
+++ Lab-1/index.php
@@ -0,0 +1,12 @@
+<?php
+    $uname="";
+    $err_uname="";
+    $pass="";
+    $err_pass="";
+    $hasError=false;
+    $users=array("tanvir"=>"1234","shium"=>"5678","karim"=>"9101");
+    if($_SERVER["REQUEST Method"]=="POST")
+    {
+        if(empty)
+    }
+?>
